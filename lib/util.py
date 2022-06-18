2850f4176409d2ac98defaafe971e6d29e602187
GitHaver <103291012+GitHaver@users.noreply.github.com>
2022-06-18 16:04:34 +1000

    List printer now working.

--- lib/util.py
+++ lib/util.py
@@ -1,9 +1,23 @@
-from os import system
+from os import chdir, system, path, replace, makedirs, remove, getcwd
 from math import ceil
+import sys
+
+home = path.expanduser('~')
+
+if '\\' in getcwd():
+    struct = ["windows", "\\"]
+elif '/' in getcwd():
+    struct = ["linux", "/"]
+else:
+    print("Operating system not recognized, please fill in the blanks:")
+    struct = [input("Operating system: ")]
+    print('What symbol is used to divide folders in a filepath?')
+    struct.append(input('E.g. In your system, what is the "/" in: "C:/Users/Home": '))
+
 
 def title(name, opt=1):
     bar = ''
-    for i in range(len(name)):
+    for i in range(len(str(name))):
         bar += '-'
     if opt == 1:
         print(bar)
@@ -11,107 +25,223 @@ def title(name, opt=1):
     print(bar)
 
 
-def cls(struct):
-    if struct[1] == "windows":
+def cls():
+    if struct[0] == "windows":
         system('cls')
-    elif struct[1] == "linux":
+    elif struct[0] == "linux":
         system('clear')
 
 
+def longest_item_in_list(item_list):
+    longest = 0
+    for item in item_list:
+        if len(item) > longest:
+            longest = len(item)
+    return longest
+
+
+def list_item_shortener(items, max_chars):
+    shortened_items = []
+    index = (2 * (int(ceil(max_chars / 5)))) - 1
+    negative_index = 0 - index
+    word_limit = 9
+    for unit in items:
+        unit = str(unit)
+        shortened_words = ""
+        if len(unit) > max_chars:
+            if len(unit.split()) > 1:
+                word_shortened = 0
+                for word in unit.split():
+                    if len(word) >= word_limit:
+                        word = word[:3] + "~ "
+                        shortened_words += word
+                        word_shortened = 1
+                    else:
+                        shortened_words += word + " "
+                if word_shortened == 0:
+                    unit = unit[:index] + "~" + unit[negative_index:]
+                else:
+                    unit = shortened_words[:-1]
+                    word_shortened = 0
+            else:
+                unit = unit[:index] + "~" + unit[negative_index:]
+        shortened_items.append(unit)
+    return shortened_items
+
+
 def list_printer(items, count=0):
-    count = count
-    list_a = []
-    for item in items:
-        item = f'[{count}] - {item}'
-        length = len(item)
-        if len(item) >= length:
-            longest = length
-        list_a.append(item)
-        count += 1
-    longest += 12
-    items = []
-    for item in list_a:
-        chars = len(item)
-        difference_from_longest = longest - chars
-        space = ' ' * difference_from_longest
-        items.append(item + space)
-    columns = ceil(len(items) / 6)
-    rows = columns
-    count = 0
-    record_id = 0
-    row_1 = ""
-    while count < columns:
-        try:
-            row_1 = row_1 + items[record_id]
-            count += 1
-            record_id += 5
-        except IndexError:
-            break
+    debugger = 0
+    formatted_items = []
+    items_per_row = 5
+    max_chars = 15
 
-    count = 0
-    record_id = 1
-    row_2 = ""
-    while count < columns:
-        try:
-            row_2 = row_2 + items[record_id]
-            count += 1
-            record_id += 5
-        except IndexError:
-            break
+    list_count = count
+    for item in list_item_shortener(items, max_chars):
+        item = f'{list_count}| {item}'
+        list_count += 1
+        formatted_items.append(item)
 
-    count = 0
-    record_id = 2
-    row_3 = ""
-    while count < columns:
-        try:
-            row_3 = row_3 + items[record_id]
-            count += 1
-            record_id += 5
-        except IndexError:
-            break
+    longest = longest_item_in_list(formatted_items)
+    lengthened_items = []
+    for item in formatted_items:
+        lengthened_items.append(item + (" " * ((longest - len(item)) + 1)))
 
-    count = 0
-    record_id = 3
-    row_4 = ""
-    while count < columns:
-        try:
-            row_4 = row_4 + items[record_id]
-            count += 1
-            record_id += 5
-        except IndexError:
-            break
+    number_of_rows = ceil(len(lengthened_items) / items_per_row)
 
-    count = 0
-    record_id = 4
-    row_5 = ""
-    while count < columns:
-        try:
-            row_5 = row_5 + items[record_id]
-            count += 1
-            record_id += 5
-        except IndexError:
-            break
-
-    count = 0
-    record_id = 5
-    row_6 = ""
-    while count < columns:
-        try:
-            row_6 = row_6 + items[record_id]
-            count += 1
-            record_id += 5
-        except IndexError:
-            break
-
-    print(row_1)
-    print(row_2)
-    print(row_3)
-    print(row_4)
-    print(row_5)
-    print(row_6)
-    return len(items)
+    list_index = 0
+    row_iteration = 0
+    debug = "\n"
+    debug += "Beginning large row loop:"
+    while row_iteration < number_of_rows:
+        debug += "\n \t"
+        row = ""
+        debug += "Beginning row " + str(row_iteration) + " loop: \n \t"
+        item_index = row_iteration
+        item_loop = 0
+        while item_loop < items_per_row:
+            try:
+                row += (lengthened_items[item_index])
+                item_index += number_of_rows
+            except IndexError:
+                debug += "\tIndex error hitting " + str(item_index) + ", row " + str(row_iteration) + " is finished: \n \t \t" + row + "\n \t"
+                break
+            item_loop += 1
+            list_index += 1
+        #debug += "Ending row " + str(row_iteration) + " loop."
+        row_iteration += 1
+        print(row)
+    debug += "\n Ending outer while loop."
+    if debugger == 1:
+        print(debug)
+    return list_count
 
 
+def take_action(cwd, struct):
+    title("Take Action", 1)
+    half_path = cwd + struct[1]
+    print('1 - Create Folder')
+    print('2 - Rename/Move Folder')
+    print('3 - Create File')
+    print('4 - Rename/Move File')
+    print('5 - Back')
+    print()
+    choice = int(input('Please choose: '))
+
+    if choice == 1:
+        print("Please enter a folder name.")
+        print(f'You may enter multiple subdirectories separated by {struct[1]}, eg:')
+        print(f"new_folder1{struct[1]}newer_folder2")
+        print("Or enter cancel to cancel.")
+        name = input(f'{half_path}')
+        if name.lower() == "cancel":
+            take_action(cwd)
+        else:
+            full_path = half_path + name
+            makedirs(f'{full_path}')
+            print(f"Success, created: {half_path}--{name}-- ")
+            print("Move to directory now?")
+            choice = input("Y/n: ")
+            if choice.lower() == "y":
+                chdir(f'{full_path}')
+                return
+            else:
+                return
+
+    elif choice == 2:
+        print("Please enter the name of a folder to delete.")
+        print("You may enter multiple subdirectories followed by a slash, e.g.:")
+        print(f"existing_directory{struct[1]}existing_subdirectory{struct[1]}folder_to_delete")
+        print("Or enter cancel to cancel.")
+        name = input(f"{half_path}")
+        if name == "cancel".lower():
+            take_action(cwd)
+        else:
+            remove(f'{half_path}{name}')
+            print(f"Success, created: {half_path}--{name}-- ")
+            wait = input("Return?")
+            menu_manager()
+
+    elif choice == 3:
+        print("Please enter a file name.")
+        print("You may enter multiple subdirectories followed by a slash, e.g.:")
+        print(f"existing_folder{struct[1]}not_existing_folder{struct[1]}filename.py")
+        print("Or enter cancel to cancel.")
+        name = input("File name: ")
+        if name == "cancel".lower():
+            take_action(cwd)
+        else:
+            with open(f'{half_path}{name}', 'w') as fp:
+                pass
+            print(f"Success, created: {half_path}-{name}- ")
+            return
+
+    elif choice == 3:
+        print("Please enter the name of the file you would like to move, rename, or delete: ")
+        print("Or enter cancel to cancel.")
+        name = input("File name: ")
+        if name == "cancel".lower():
+            take_action(cwd)
+        else:
+            print()
+            print("Please enter the new path of the file including the file, with its new name: ")
+            print("This may include folders that do not exist yet, e.g: ")
+            print(f"new_dir{struct[1]}newer_dir{struct[1]}new_file_name.txt")
+            newfile = input(f'{half_path}')
+            replace(f'{half_path}', f'{half_path}{newfile}')
+
+    elif choice == 4:
+        back()
 
 
+def dir_nav_chooser(cwd, choices):
+    choice = int(input(f'{cwd}: '))
+    chosen_item = choices[choice]
+    if chosen_item == '<':
+        chdir("..")
+        return
 
+    elif chosen_item == "Exit":
+        sys.exit()
+
+    elif chosen_item == "Home":
+        chdir(home)
+        return
+
+    elif chosen_item == "takeaction":
+        take_action(cwd, struct)
+        return
+
+    elif chosen_item[0] == 'F':
+        item = chosen_item.lstrip('F')
+        chdir(item)
+        return
+
+    elif chosen_item[0] =='f':
+        choice = chosen_item.lstrip('f')
+        file_handler(choice)
+
+
+def file_handler(choice):
+    if '.py' in choice:
+        print(f"1. Run {choice}")
+        print(f"2. Edit {choice}")
+        choice2 = input("Please choose: ")
+        if choice2 == '1':
+            system(f"python {choice}")
+        elif choice2 == '2':
+            system(f"sudo nano {choice}")
+
+    elif '.js' in choice:
+        print(f"1. Run {choice}")
+        print(f"2. Edit {choice}")
+        choice2 = input("Please choose: ")
+        if choice2 == '1':
+            system(f"node {choice}")
+        elif choice2 == '2':
+            system(f"sudo nano {choice}")
+        else:
+            system(f"sudo nano {choice}")
+
+    else:
+        print(f"No handler found for {choice} - sending to Nano")
+        system(f"sudo nano {choice}")
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from os import chdir, system, getcwd, path, listdir, replace, makedirs, remove, scandir
-from lib.util import title, cls, list_printer
-from sys import exit
+from os import chdir, getcwd, path
+from lib.util import title, cls, list_printer, dir_nav_chooser
 from pathlib import Path
+import sys
 
 home = path.expanduser('~')
-chdir(home)
+
+try:
+    chdir(str(sys.argv[1]))
+except IndexError:
+    chdir(home)
+cwd = getcwd()
 
 if '\\' in getcwd():
     struct = ["windows", "\\"]
@@ -17,188 +22,54 @@ else:
     struct.append(input('E.g. In your system, what is the "/" in: "C:/Users/Home": '))
 
 
-def back():
-    menu_manager()
-
-
-def take_action(cwd):
-    title("Take Action", 1)
-    half_path = cwd + {struct[2]}
-    print('1 - Create Folder')
-    print('2 - Rename/Move Folder')
-    print('3 - Create File')
-    print('4 - Rename/Move File')
-    print('5 - Back')
-    print()
-    choice = int(input('Please choose: '))
-
-    if choice == 1:
-        print("Please enter a folder name.")
-        print(f'You may enter multiple subdirectories separated by {struct[2]}, eg:')
-        print(f"new_folder1{struct[2]}newer_folder2")
-        print("Or enter cancel to cancel.")
-        name = input(f'{half_path}')
-        if name.lower() == "cancel":
-            take_action(cwd)
-        else:
-            full_path = half_path + name
-            makedirs(f'{full_path}')
-            print(f"Success, created: {half_path}--{name}-- ")
-            print("Move to directory now?")
-            choice = input("Y/n: ")
-            if choice.lower() == "y":
-                chdir(f'{full_path}')
-                menu_manager()
-            else:
-                menu_manager()
-
-    elif choice == 2:
-        print("Please enter the name of a folder to delete.")
-        print("You may enter multiple subdirectories followed by a slash, e.g.:")
-        print(f"existing_directory{struct[2]}existing_subdirectory{struct[2]}folder_to_delete")
-        print("Or enter cancel to cancel.")
-        name = input(f"{half_path}")
-        if name == "cancel".lower():
-            take_action()
-        else:
-            remove(f'{half_path}{name}')
-            print(f"Success, created: {half_path}--{name}-- ")
-            wait = input("Return?")
-            menu_manager()
-
-    elif choice == 3:
-        print("Please enter a file name.")
-        print("You may enter multiple subdirectories followed by a slash, e.g.:")
-        print(f"existing_folder{struct[2]}not_existing_folder{struct[2]}filename.py")
-        print("Or enter cancel to cancel.")
-        name = input("File name: ")
-        if name == "cancel".lower():
-            take_action(cwd)
-        else:
-            with open(f'{half_path}{name}', 'w') as fp:
-                pass
-            print(f"Success, created: {half_path}-{name}- ")
-            wait = input("Return?")
-            menu_manager()
-
-    elif choice == 3:
-        print("Please enter the name of the file you would like to move, rename, or delete: ")
-        print("Or enter cancel to cancel.")
-        name = input("File name: ")
-        if name == "cancel".lower():
-            take_action(cwd)
-        else:
-            print()
-            print("Please enter the new path of the file including the file, with its new name: ")
-            print("This may include folders that do not exist yet, e.g: ")
-            print(f"new_dir{struct[2]}newer_dir{struct[2]}new_file_name.txt")
-            newfile = input(f'{half_path}')
-            replace(f'{half_path}', f'{half_path}{newfile}')
-
-    elif choice == 4:
-        back()
-
-
-def menu_choice(contents):
-    choice = int(input("Please choose: "))
-    choice -= 1
-    choice = contents[choice]
-    choice = choice.replace(",","")
-
-    if choice == '<':
-        chdir("..")
-        menu_manager()
-
-    elif choice == "Home":
-        chdir(home)
-        menu_manager()
-
-    elif choice == "Exit":
-        exit()
-
-    elif choice == "take_action":
-        take_action(cwd)
-
-    elif choice[0] =='F':
-          chdir(choice.lstrip('F'))
-          menu_manager()
-
-    elif choice[0] =='f':
-        choice = choice.lstrip('f')
-
-        if '.py' in choice:
-            print(f"1. Run {choice}")
-            print(f"2. Edit {choice}")
-            choice2=input("Please choose: ")
-            if choice2 == '1':
-                system(f"python {choice}")
-            elif choice2 == '2':
-                system(f"sudo nano {choice}")
-
-        elif '.js' in choice:
-            print(f"1. Run {choice}")
-            print(f"2. Edit {choice}")
-            choice2=input("Please choose: ")
-            if choice2 == '1':
-                system(f"node {choice}")
-            elif choice2 == '2':
-                system(f"sudo nano {choice}")
-            else:
-                system(f"sudo nano {choice}")
-
-        else:
-            print(f"No handler found for {choice} - sending to Nano")
-            system(f"sudo nano {choice}")
-
-
-def menu_manager():
-    cls(struct)
+def menu_manager(directory=getcwd()):
+    cls()
+    chdir(directory)
     contents = []
-    cwd = getcwd()
+    title(directory)
 
     f = []
     d = []
+
     count = 0
-
-    folders = [d for d in Path('.').iterdir() if d.is_dir()]
+    folders = [d for d in Path(".").iterdir() if d.is_dir()]
     for folder in folders:
-        count += 1
         contents.append(f'F{folder}')
-
-    files = [f for f in Path('.').iterdir() if f.is_file()]
-    for file in files:
         count += 1
-        contents.append(f'f{file}')
-
-    title(cwd)
-
     print("Folders:")
     print()
     count = list_printer(folders)
+
+    files = [f for f in Path(".").iterdir() if f.is_file()]
+    for file in files:
+        contents.append(f'f{file}')
+        count += 1
+    print()
     print("Files:")
     print()
-    list_printer(files, count)
+    count = list_printer(files, count)
 
-    count = 0
     print()
     print("Actions:")
-    count += 1
     contents.append('takeaction')
     print(f'{count} - Take Action')
     count += 1
     contents.append('<')
     print(f'{count} - <')
-    if path.expanduser('~') == cwd:
+    if home == cwd:
         either = "Exit"
     else:
         either = "Home"
     contents.append(either)
     count += 1
+    print(f'{count} - {either}')
+    dir_nav_chooser(cwd, contents)
 
-    menu_choice(contents)
 
 while __name__ == "__main__":
-    menu_manager()
-
-
+    try:
+        menu_manager(getcwd())
+    except PermissionError:
+        wait = input("You do not have access to this folder, press any key to return to your home directory.")
+        menu_manager(home)
 
